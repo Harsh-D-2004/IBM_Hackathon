e439bffa17944ca876f232e74bf4eb898063c1c9
Harsh Doshi <119748985+Harsh-D-2004@users.noreply.github.com>
2025-01-21 18:50:50 +0530

    Upp

---
+++ Notebooks/cors.py
@@ -0,0 +1,10 @@
+from ibmcloudant.cloudant_v1 import CloudantV1
+
+service = CloudantV1.new_instance()
+
+response = service.put_cors_configuration(
+  enable_cors=True,
+  origins=['*']
+).get_result()
+
+print(response)
